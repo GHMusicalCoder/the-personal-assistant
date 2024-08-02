d05934c413149b7963a1a1fc8e64deaf9534dfc9
GHMusicalCoder <musical.coder@thekiralys.us>
2024-08-01 21:12:23 -0400

    AUG01.4 - renaming linux magazine files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # the-personal-assitant
 This is a computerized personal assistant that will download things for me, rename files for me, and all around computer chore automation assistant.
+
+## Things I want to done:
+* Go through lists of folders (from settings) and Linux Magazine files
--- pia.py
+++ pia.py
@@ -1,14 +1,37 @@
-import sys
+import os
 import yaml
+import fnmatch
+import shutil
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
-f_download = ""
+
+def linux_magazine():
+    for folder in settings["rename_folders"]:
+        for file in os.listdir(settings['home_folder']+folder):
+            if fnmatch.fnmatch(file.lower(), "lm_*.pdf"):
+                rename_linux_magazine(settings['home_folder']+folder, file)
+
+
+def rename_linux_magazine(folder, file):
+    issue = int(file.split(".")[0].split("_")[1])
+    months = issue - 282
+    the_date = datetime(2024, 5, 1) + relativedelta(months=months)
+    year = the_date.year
+    month = the_date.strftime("%m")
+    rename = f"{year}-{month}_Linux_Pro_Magazine_{issue}.pdf"
+    shutil.move(f"{folder}/{file}", f"{settings['linux_magazine_location']}/{year}/{rename}")
 
 
 def main():
-    pass
+    print("Processing Linux Magazine downloads")
+    linux_magazine()
 
 
 if __name__ == "__main__":
+    # testing
+    # rename_linux_magazine("", "LM_283.pdf")
+
     try:
         with open('settings.yaml', 'r') as f:
             settings = yaml.safe_load(f)
@@ -17,3 +40,5 @@ if __name__ == "__main__":
         print("Please create a settings.yaml file for this program to use.")
         print("See GitHub documentation for further assistance.")
         exit()
+
+    main()
